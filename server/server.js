6a62d7352c97d0b30c7e47d1e64547b1cf56b72b
Michael Camden <camden.michael@gmail.com>
2021-10-27 22:12:17 -0400

    implement controller in server, update express to handle json data

--- server/server.js
+++ server/server.js
@@ -1,11 +1,45 @@
+const {
+  saveSleepData,
+  getSleepDataByIndex,
+  listSleepData,
+} = require('./controllers/SleepData');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(bodyParser.json()); // for parsing application/json
+
+// Find some sleep data
+app.get('/sleepData(/:index)?', (req, res) => {
+  const index = req.params.index;
+  let data;
+
+  if (!index) {
+    data = listSleepData();
+  } else {
+    data = getSleepDataByIndex(index);
+  }
+
+  // Catching here to ensure we're always responding
+  try {
+    res.send(JSON.stringify(data));
+  } catch (e) {
+    res.send('Unable to parse data', e.message);
+  }
+});
+
+// Store new sleep data
+app.post('/sleepData', (req, res) => {
+  const result = saveSleepData(req.body);
+
+  res.send(
+    JSON.stringify({
+      result,
+    })
+  );
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`SleepyTime server listening at http://localhost:${port}`);
 });
